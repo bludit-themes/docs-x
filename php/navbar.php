59884c7c0274455c1f09fc13bc4091ac8442e88f
Edi <egoetschel@clickwork.ch>
2020-08-17 23:08:57 +0200

    Update navbar.php

--- php/navbar.php
+++ php/navbar.php
@@ -1,11 +1,9 @@
 <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark text-uppercase">
 	<div class="container-fluid">
+	    <div class="b-burger-btn"><i></i><b></b></div>
 		<a class="navbar-brand" href="<?php echo $site->url() ?>">
 			<span class="text-white"><?php echo $site->title() ?></span>
 		</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
 		<div class="collapse navbar-collapse" id="navbarResponsive">
 			<ul class="navbar-nav ml-auto">
 
